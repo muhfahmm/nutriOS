6fd45d4a4053ed716d49314e690d8e4ac1b76c03
Muhfahmm <fahimfahim0407@gmail.com>
2026-08-07 15:39:53 +0700

    Initial commit
    Generated by create-expo-app 4.0.0.

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.greeting}>Hello user</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  greeting: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+});
